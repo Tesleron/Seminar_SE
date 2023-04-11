5acbfbcbb400f7aef7f6235cb9336eeee5975118
Ga <zeriona10@gmail.com>
2023-04-12 01:26:41 +0300

    Add new images and logic

--- View.py
+++ View.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import math
 import time
-import threading
+import textwrap
+from PIL import Image, ImageTk
 
 
 def yp(n, m, k, view, interval, i = 0):
     # 0<n     n elements in the circle
-    hi = 2 #dogshit
     # 1<=m<=n    reduce the m element after the current existing element
     # 1<=k<=n k elements remain in the circle at the end of process
     print("n =", n, " m =", m, " k =", k)
@@ -22,12 +22,16 @@ def yp(n, m, k, view, interval, i = 0):
             l.remove(l[j])
         view.master.update()
         time.sleep(interval)
-        # removal from gui
+        
     if m == 1 and k==1 and view.active:
+     message = "n = " + str(n) + " survive by algorithm = " + str(l) + ", survive by formula = " + str(int(2 * (n - math.pow(2, math.floor(math.log(n, 2)))) + 1))
+     view.display_endgame_msg(message)
      print("n =", n, "survive by algorithm =", l, \
            "survive by formula =", \
            int(2 * (n - math.pow(2, math.floor(math.log(n, 2)))) + 1))
     elif view.active:
+        message = "n = " + str(n) + " survive by algorithm = " + str(l)
+        view.display_endgame_msg(message)
         print("n =", n, "survive by algorithm =", l)
     else:
         print("Game canceled")
@@ -98,7 +102,8 @@ class CircleNumbers:
         # Create the canvas for the circle
         self.canvas = tk.Canvas(master, width=450, height=450)
         self.canvas.pack()
-
+        self.canvas.image_refs = []
+        
         # Calculate the center coordinates of the root window
         root_width = root.winfo_width()
         root_height = root.winfo_height()
@@ -114,13 +119,18 @@ class CircleNumbers:
         self.playButton = tk.Button(frame, text="Start", command=self.start_timer, state='disabled')
         self.playButton.pack(pady=2)
 
-        # Create the "PLAY" button
-        self.button = tk.Button(frame, text="Restart", command=self.clear_circle)
+        # Create the "Restart" button
+        self.button = tk.Button(frame, text="Restart", command=lambda: self.clear_circle(True))
         self.button.pack(pady=2)
+        
+        self.endgame_text = tk.Label(self.master, text="", font=("Arial", 12))
+        self.endgame_text.place(relx = 0.0, rely = 1.0, anchor ='sw')
 
         # Initialize variables
         self.numbers = {}
-        self.circle_size = 150  # radius of the circle
+        self.images = {}
+        self.circle_size = 190  # radius of the circle
+        self.circle_size_imgs = 160  # radius of the images circle
         self.circle_center = (200, 250)  # center of the circle
         self.timer = None
 
@@ -133,7 +143,7 @@ class CircleNumbers:
         yp(n, m, k, self, interval, i)
 
     def draw_circle(self, n):
-        self.clear_circle()
+        self.clear_circle(False)
         self.active = True
         n = int(n)
         self.sliderStartFrom.config(to=n)
@@ -144,24 +154,59 @@ class CircleNumbers:
         else:
             self.playButton.config(state='disabled')
             return
+        
+        self.draw_outer_circle(n)
+        self.draw_inner_circle(n)
+            
+    def draw_outer_circle(self, n):
         # Calculate the positions of the numbers in the circle
         angle_increment = 2 * math.pi / n
         for i in range(n):
             angle = i * angle_increment
             x = self.circle_center[0] + self.circle_size * math.cos(angle)
             y = self.circle_center[1] + self.circle_size * math.sin(angle)
+            
             number_label = self.canvas.create_text(x, y, text=str(i+1))
             self.numbers[i+1] = number_label
+            
+            
+    def draw_inner_circle(self, n):
+        # Calculate the positions of the numbers in the circle
+        angle_increment = 2 * math.pi / n
+        for i in range(n):
+            angle = i * angle_increment
+            x_img = self.circle_center[0] + self.circle_size_imgs * math.cos(angle)
+            y_img = self.circle_center[1] + self.circle_size_imgs * math.sin(angle)
+            
+            img = Image.open("C:\PythonGal\Seminar_SE\gladiator (3).png")
+            photo = ImageTk.PhotoImage(img)
+            self.canvas.image_refs.append(photo)
+            photo_output = self.canvas.create_image(x_img, y_img, image=photo)
+            self.images[i+1] = photo_output
+            
+            
 
     def remove_numbers(self, number_to_remove):
         self.canvas.delete(self.numbers[number_to_remove])
+        self.canvas.delete(self.images[number_to_remove])
         del self.numbers[number_to_remove]
+        del self.images[number_to_remove]
                    
-    def clear_circle(self):
+    def clear_circle(self, from_restart):
+        if (from_restart):
+            self.sliderPeople.set(0)
+        self.endgame_text.config(text = "")
         self.active = False
         for number_label in self.numbers.values():
             self.canvas.delete(number_label)
         self.numbers = {}
+        self.images = {}
+        self.canvas.image_refs = []
+        self.canvas.delete('all')
+        
+    def display_endgame_msg(self, message):
+        message = textwrap.fill(message, width=70)
+        self.endgame_text.config(text = message)   
 
 root = tk.Tk()
 app = CircleNumbers(root)
